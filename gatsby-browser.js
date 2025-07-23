6708541d0de60d122f1bfcf636e9bd560bcee6a6
heyitssakinah <contact@rizqx.sg>
2025-07-23 16:03:50 +0800

    Add existing files to git

---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+import "@fontsource/dm-sans"
+import "@fontsource/dm-sans/500.css"
+import "@fontsource/dm-sans/700.css"
+import "@fontsource/dm-mono"
+import "@fontsource/dm-mono/500.css"
+import "@fontsource/montserrat"
+import "@fontsource/cabin"
+import "@fontsource/literata"
+
---
+++ src/colors.css.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  background: "#456143",
+  text: "#fbf7ba",
+  primary: "#fbf7ba",
+  muted: "#74776b",
+  active: "#dcd5cf",
+  black: "#000",
+}
